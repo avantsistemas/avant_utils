f79205870536b51ff00b52f69b78e8ee3f9a1da1
Lucas Repolês <lucas@avantsistemas.com>
2020-08-20 16:06:58 -0300

    Adiciona módulo para salvar parâmetros na sessão

--- lib/avant_utils.rb
+++ lib/avant_utils.rb
@@ -2,6 +2,7 @@ module AvantUtils
   require 'avant_utils/cnpj'
   require 'avant_utils/cpf'
   require 'avant_utils/url'
+  require 'avant_utils/params_storage'
   require 'avant_utils/previous_location'
   require 'avant_utils/active_model/cnpj_validator'
   require 'avant_utils/active_model/cpf_validator'
