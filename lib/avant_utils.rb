ae848f3c14519ddb4aba37377a2022754c2f304d
Lucas Repolês <lucas@avantsistemas.com>
2020-08-27 14:42:21 -0300

    Implementa validador de URL

--- lib/avant_utils.rb
+++ lib/avant_utils.rb
@@ -6,5 +6,6 @@ module AvantUtils
   require 'avant_utils/previous_location'
   require 'avant_utils/active_model/cnpj_validator'
   require 'avant_utils/active_model/cpf_validator'
+  require 'avant_utils/active_model/url_validator'
   I18n.load_path += Dir[File.join(__dir__, 'avant_utils/locale/*.yml')]
 end
---
+++ lib/avant_utils/active_model/url_validator.rb
@@ -0,0 +1,15 @@
+module ActiveModel
+  module Validations
+    class UrlValidator < EachValidator
+      def validate_each(record, attribute, value)
+        return if value.blank? && options[:allow_blank]
+
+        return if value.nil? && options[:allow_nil]
+
+        return if AvantUtils::Url.valid?(value)
+
+        record.errors.add(attribute, :invalid_url, message: options[:message], value: value)
+      end
+    end
+  end
+end
