87d8e94fb4cb79ac083d286f5c1bdbf1da2930ff
Lucas Repolês <lucas@avantsistemas.com>
2021-02-26 17:59:23 -0300

    Permite que subclasses possam redefinir o escopo padrão da pesquisa
    Essa modificação permite que as subclasses possam alterar o escopo da
    pesquisa antes dela ser realizada. Por exemplo, quando há um campo
    boleano no formulário de pesquisa, que vem desmarcado, por padrão, pode
    ser desejável alterar o escopo nessa situação.
    
    Exemplo concreto: numa tela de usuários, há no filtro de pesquisa o
    checkbox "incluir usuários inativos". Ao abrir a tela, queremos que
    somente os usuários ativos sejam incluídos na pesquisa, por isso o
    checkbox vem desmarcado, por padrão. Os usuários inativos serão
    incluídos somente quando esse checkbox for marcado.
    
    A subclasse UserSearch poderia redefinir o método
    `redefine_scope` para tratar essa situação:
    
    ```ruby
    def redefine_scope
      include_inactive? ? scope : scope.where(status: :active)
    end
    ```

--- lib/avant_utils/application_search.rb
+++ lib/avant_utils/application_search.rb
@@ -1,22 +1,20 @@
 module AvantUtils
+  # Facilita a construção de formulários de pesquisa/filtro.
   class ApplicationSearch
     private_class_method :new
 
     def initialize(scope)
-      @scope = scope
+      define_scope(scope)
       @empty = true
     end
 
-    def self.scope(scope = default_scope)
+    def self.scope(scope)
       new(scope)
-    end
-
-    def self.default_scope
-      raise NotImplementedError, "default_scope não implementado em #{name}"
     end
 
     def search(attributes)
       narrow_search(attributes) if attributes.present?
+      define_scope(redefine_scope, by: :redefine_scope)
       self
     end
 
@@ -42,6 +40,12 @@ module AvantUtils
 
     attr_reader :scope
 
+    # Subclasses podem sobreescrever essa classe para redefinir
+    # o escopo da pesquisa com base nos atributos de pesquisa.
+    def redefine_scope
+      @scope
+    end
+
     def narrow_search(attributes)
       attributes.each do |name, value|
         search_method = "#{name}_search"
@@ -56,8 +60,19 @@ module AvantUtils
     end
 
     def narrow_scope(search_method, value)
-      @scope = send(search_method, value)
+      new_scope = send(search_method, value)
+      define_scope(new_scope, by: search_method)
       @empty = false
+    end
+
+    def define_scope(scope, by: nil)
+      unless scope.is_a?(ActiveRecord::Relation)
+        message = ["Tentativa de definição do escopo para #{scope || 'nil'}."]
+        message << ['O escopo precisa ser um objeto ActiveRecord::Relation.']
+        message << ["Verifique o método `#{by}` de #{self.class}."] if by
+        raise message.join(' ')
+      end
+      @scope = scope
     end
   end
 end
