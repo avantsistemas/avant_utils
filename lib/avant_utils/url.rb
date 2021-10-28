42d349717252a1ac512e64cb61fde49be5c3f6e2
Lucas Repolês <lucas@avantsistemas.com>
2021-10-28 19:21:32 -0300

    Acata sugestão de alteração do Rubocop

--- lib/avant_utils/url.rb
+++ lib/avant_utils/url.rb
@@ -1,7 +1,7 @@
 module AvantUtils
   class Url
     def self.valid?(value)
-      regexp = %r(https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&/=]*))
+      regexp = %r(https?://(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&/=]*))
       !(value =~ regexp).nil?
     end
 
