7f6fa0a7abac4373a3f234976c906521b4c86bc9
Lucas Repolês <lucas@avantsistemas.com>
2022-04-08 21:04:18 -0300

    Revisa validador de CEP

--- lib/avant_utils/active_model/zip_validator.rb
+++ lib/avant_utils/active_model/zip_validator.rb
@@ -5,12 +5,13 @@ module ActiveModel
     class ZipValidator < EachValidator
       def validate_each(record, attribute, value)
         return if value.blank? && options[:allow_blank]
-
         return if value.nil? && options[:allow_nil]
+        return if AvantUtils::Constants::EMAIL_FORMAT.match?(value)
 
-        return if /\A\d{5}-\d{3}\z/.match?(value.to_s)
-
-        record.errors.add(attribute, :invalid_zip, message: options[:message], value: value)
+        record.errors.add(attribute,
+                          :invalid_zip,
+                          message: options[:message],
+                          value: value)
       end
     end
   end
