02709e285b165d71524c40c280d006dd68f5bc3b
Lucas Repolês <lucas@avantsistemas.com>
2022-04-27 10:08:10 -0300

    Simplifica regexp com sugestão do RubyMine

--- lib/avant_utils/constants.rb
+++ lib/avant_utils/constants.rb
@@ -5,9 +5,9 @@ module AvantUtils
     CNPJ_FORMAT = %r{\A\d{2}\.\d{3}\.\d{3}/\d{4}-\d{2}\z}.freeze
     CPF_FORMAT = /\A\d{3}\.\d{3}\.\d{3}-\d{2}\z/.freeze
     EMAIL_FORMAT = /\A\s*([-\p{L}\d+._]{1,64})@((?:[-\p{L}\d]+\.)+\p{L}{2,})\s*\z/i.freeze
-    LICENSE_PLATE_FORMAT = /\A[A-Z]{3}-\d[A-Z0-9]\d\d\z/.freeze
+    LICENSE_PLATE_FORMAT = /\A[A-Z]{3}-\d[A-Z\d]\d\d\z/.freeze
     PHONE_FORMAT = /\A\d{2}\s\d{4,5}-\d{4}\z/.freeze
-    URL_FORMAT = %r(https?://(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&/=]*)).freeze
+    URL_FORMAT = %r(https?://(www\.)?[-a-zA-Z\d@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z\d@:%_+.~#?&/=]*)).freeze
     ZIP_FORMAT = /\A\d{5}-\d{3}\z/.freeze
   end
 end
