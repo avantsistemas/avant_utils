6506b699203d9f7d5201981d6813f1fb4b4d9348
Lucas Repolês <lucas@avantsistemas.com>
2022-04-28 15:26:24 -0300

    Adiciona classes com utilitários para exportação de relatórios

---
+++ lib/avant_utils/axlsx/styles.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+module AvantUtils
+  module Axlsx
+    class Styles
+      def initialize(workbook)
+        @workbook = workbook
+      end
+
+      def title
+        @title ||= @workbook.styles.add_style b: true, sz: 16
+      end
+
+      def subtitle
+        @subtitle ||= @workbook.styles.add_style sz: 14
+      end
+
+      def table_header
+        @table_header ||= @workbook.styles.add_style b: true, bg_color: 'ffc107', border: { style: :thin, color: '00',
+                                                                                            edges: %i[bottom] }
+      end
+
+      def table_footer
+        @table_footer ||= @workbook.styles.add_style b: true, bg_color: 'fff6bf'
+      end
+
+      def table_footer_currency
+        @table_footer_currency ||= @workbook.styles.add_style b: true, bg_color: 'fff6bf', num_fmt: 8
+      end
+
+      def bold
+        @bold ||= @workbook.styles.add_style b: true
+      end
+
+      def currency
+        @currency ||= @workbook.styles.add_style num_fmt: 8
+      end
+
+      def datetime
+        @datetime ||= @workbook.styles.add_style num_fmt: 22
+      end
+
+      def percent
+        @percent ||= @workbook.styles.add_style num_fmt: Axlsx::NUM_FMT_PERCENT
+      end
+    end
+  end
+end
